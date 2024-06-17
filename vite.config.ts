1c2fee42e20a34ec45932b1763471433d7025482
Tom Grossi <TheAutomaTom@gmail.com>
2024-06-17 14:10:36 -0500

    Work in progress

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,53 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {VitePWA} from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      injectRegister: 'auto', // https://vite-pwa-org.netlify.app/guide/register-service-worker.html
+      registerType: 'autoUpdate',
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true,
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+
+      },
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: 'Lab.Pwa-Deploy',
+        short_name: 'PwaDeploy',
+        description: 'Testing my Pwa Deploy setup ✌️',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: '512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: '512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    })
+  ],
 })
