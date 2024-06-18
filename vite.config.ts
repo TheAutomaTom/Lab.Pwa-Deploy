db310ee12bf417f3c98d38b60b5d528f597f0434
Tom Grossi <TheAutomaTom@gmail.com>
2024-06-18 07:37:26 -0500

    Add subdomain config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import {VitePWA} from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: "./",
+  build: {
+    outDir: "dist",
+  },
+  base: "/Pwa-Test-Harness/",
+  publicDir: "public",
   plugins: [
     vue(),
     VitePWA({
